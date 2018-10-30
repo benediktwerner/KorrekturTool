4a72fc15740c3a4d2199a3e12c978cb88bf11dfb
Benedikt Werner <1benediktwerner@gmail.com>
2018-10-30 17:07:21 +0100

    Disable electron security warnings

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+window.ELECTRON_DISABLE_SECURITY_WARNINGS = true;
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
